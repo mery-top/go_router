b82150fd6741039beb49e83f88c6867e633c226a
Meerthika <meerthikasrrajendrans@gmail.com>
2025-04-25 15:45:45 +0530

    Add ForwardPacket

--- routers/staticRouter.go
+++ routers/staticRouter.go
@@ -10,23 +10,39 @@ type Packet struct{
 	Data string
 }
 
-type routingTable map[string]string
+type RoutingTable map[string]string
 
 func(p *Packet) String() string{
-	return fmt.Sprintf("Packet:{Source: %s, Destination: %s, Data %s}", p.Source, p.Destination, p.Data)
+	return fmt.Sprintf("Packet: Source: %s, Destination: %s, Data: %s \n", p.Source, p.Destination, p.Data)
 }
 
+func (rt RoutingTable )GetnextHop(destination string) string{
+	nextHop, exists:= rt[destination]
+	if exists{
+		return nextHop
+	}
+	return "No Route Found"
+}
+
+
+func ForwardPacket(p *Packet, routingTable RoutingTable){
+	nextHop:= routingTable.GetnextHop(p.Destination)
+	fmt.Printf("The NextHop is %s\n", nextHop)
+	fmt.Println(p)
+}
+
+
 func main(){
-	router1RoutingTable:= routingTable{
-		"192.168.1.1" : "Router2",
+	router1RoutingTable:= RoutingTable{
+		"192.168.1.2" : "Router2",
 	}
 
-	router2RoutingTable:= routingTable{
+	router2RoutingTable:= RoutingTable{
 		"192.168.1.2": "Final Destination",
 	}
 
-	packet:= Packet{
-		Source: "192.168.0.0",
+	packet:= &Packet{
+		Source: "192.168.1.1",
 		Destination: "192.168.1.2",
 		Data: "Hello, This is a Packet",
 	}
